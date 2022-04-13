d789b1656da9596f8c8215cbb2aef9207a7d67af
monika <kmonika9407@gmail.com>
2022-04-14 03:14:09 +0530

    Appointmnt booking

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # appointmnt_bookings
+
+
+### `npm i`
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br>
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+The app is ready to be deployed!
---
+++ build/precache-manifest.6a9c877a4ae5d8032c2a3aed4bebfc92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "14b053e5bf401c14f0f2",
+    "url": "/static/css/main.7281f2ae.chunk.css"
+  },
+  {
+    "revision": "14b053e5bf401c14f0f2",
+    "url": "/static/js/main.14b053e5.chunk.js"
+  },
+  {
+    "revision": "32fca22b304465231323",
+    "url": "/static/css/1.ed8c9269.chunk.css"
+  },
+  {
+    "revision": "32fca22b304465231323",
+    "url": "/static/js/1.32fca22b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5836bc58bb0024475721626d82596ba0",
+    "url": "/static/media/logo.5836bc58.svg"
+  },
+  {
+    "revision": "a75909c00b2a07cd2fade048a806c3c5",
+    "url": "/index.html"
+  }
+];
